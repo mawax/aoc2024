e7aa7edd63ff80d0ea9f4170bcc94c3fe4a2ebd5
mawax <wagemakers.m@gmail.com>
2024-12-19 10:50:42 +0100

    Aocd11.1

---
+++ Aoc.Day11.1/Program.cs
@@ -0,0 +1,41 @@
+﻿var input = File.ReadAllText("input.txt");
+
+var stones = new LinkedList<long>(input.Split(' ').Select(long.Parse));
+
+const int blinks = 25;
+for (var i = 0; i < blinks; i++)
+{
+    var node = stones.First;
+    while (node != null)
+    {
+        if (node.Value == 0)
+        {
+            node.Value = 1;
+
+            node = node.Next;
+            continue;
+        }
+
+        if (node.Value.ToString().Length % 2 == 0)
+        {
+            var nodeString = node.Value.ToString();
+            var leftPart = nodeString[..(nodeString.Length / 2)];
+            var rightPart = nodeString[(nodeString.Length / 2)..];
+
+            node.Value = int.Parse(leftPart);
+            var newNode = new LinkedListNode<long>(long.Parse(rightPart));
+            stones.AddAfter(node, newNode);
+
+            node = newNode.Next;
+            continue;
+        }
+
+        node.Value *= 2024;
+
+        node = node.Next;
+    }
+
+    //Console.WriteLine(string.Join(' ', stones));
+}
+
+Console.WriteLine("D11.1: " + stones.Count);
